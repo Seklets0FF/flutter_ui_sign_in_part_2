803b56d5ab53675e063608c7d8628f66e8033f84
sseklecov <p0o9i8u7!>
2020-07-28 22:46:10 +0700

    add signup.dart

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,38 +1,37 @@
+import 'dart:convert';
+
+import 'main.dart';
 import 'package:flutter/material.dart';
-import 'signup.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(),
-      //home: SignUpPage(),
-    );
-  }
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State {
+class _SignUpPageState extends State {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomPaint(
-        painter: BackgroundSignIn(),
+        painter: BackgroundSignUp(),
         child: Stack(
           children: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: const EdgeInsets.only(left: 25.0, top: 35.0),
+                child: Icon(Icons.arrow_back_ios, color: Colors.white,),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 35),
               child: Column(
                 children: <Widget>[
                   _getHeader(),
                   _getInputs(),
-                  _getSignIn(),
+                  _getSignUp(),
                   _getBottomRow(context),
                 ],
               ),
@@ -50,7 +49,7 @@ _getHeader() {
     child: Container(
       alignment: Alignment.bottomLeft,
       child: Text(
-        'Добро пожаловать',
+        'Регистрация аккаунта',
         style: TextStyle(color: Colors.white, fontSize: 37),
       ),
     ),
@@ -61,16 +60,38 @@ _getInputs() {
   return Expanded(
     flex: 4,
     child: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
         TextField(
-          decoration: InputDecoration(labelText: 'Логин'),
+          decoration: InputDecoration(
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)),
+            labelText: 'Имя',
+            labelStyle: TextStyle(color: Colors.white),
+          ),
         ),
         SizedBox(
           height: 15,
         ),
         TextField(
-          decoration: InputDecoration(labelText: 'Пароль'),
+          decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              labelText: 'E-mail',
+              labelStyle: TextStyle(color: Colors.white)),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            labelText: 'Пароль',
+            labelStyle: TextStyle(color: Colors.white),
+          ),
         ),
         SizedBox(
           height: 15,
@@ -80,15 +101,16 @@ _getInputs() {
   );
 }
 
-_getSignIn() {
+_getSignUp() {
   return Expanded(
-    flex: 1,
+    flex: 2,
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Text(
-          'Вход',
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+          'Регистрация',
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w500, color: Colors.white),
         ),
         CircleAvatar(
           backgroundColor: Colors.grey.shade800,
@@ -107,34 +129,26 @@ _getBottomRow(context) {
   return Expanded(
     flex: 1,
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         GestureDetector(
           onTap: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SignUpPage()));
+            Navigator.pop(context);
           },
           child: Text(
-            'Регистрация',
+            'Вход',
             style: TextStyle(
+                color: Colors.white,
                 fontSize: 15,
                 fontWeight: FontWeight.w500,
                 decoration: TextDecoration.underline),
           ),
         ),
-        Text(
-          'Забыли пароль',
-          style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
-              decoration: TextDecoration.underline),
-        )
       ],
     ),
   );
 }
 
-class BackgroundSignIn extends CustomPainter {
+class BackgroundSignUp extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var sw = size.width;
@@ -149,32 +163,21 @@ class BackgroundSignIn extends CustomPainter {
     // Blue
     Path blueWave = Path();
     blueWave.lineTo(sw, 0);
-    blueWave.lineTo(sw, sh * 0.5);
-    blueWave.quadraticBezierTo(sw * 0.5, sh * 0.45, sw * 0.2, 0);
+    blueWave.lineTo(sw, sh * 0.65);
+    blueWave.cubicTo(sw * 0.8, sh * 0.8, sw * 0.5, sh * 0.8, sw * 0.45, sh);
+    blueWave.lineTo(0, sh);
     blueWave.close();
-    paint.color = Colors.blue.shade300;
+    paint.color = Colors.lightBlue.shade300;
     canvas.drawPath(blueWave, paint);
 
     // Grey
-    Path greyWave = Path();
-    greyWave.lineTo(sw, 0);
-    greyWave.lineTo(sw, sh * 0.1);
-    greyWave.cubicTo(
-        sw * 0.95, sh * 0.15, sw * 0.65, sh * 0.15, sw * 0.6, sh * 0.38);
-    greyWave.cubicTo(sw * 0.52, sh * 0.52, sw * 0.05, sh * 0.45, 0, sh * 0.4);
-    greyWave.close();
+    Path greyPath = Path();
+    greyPath.lineTo(sw, 0);
+    greyPath.lineTo(sw, sh * 0.3);
+    greyPath.cubicTo(sw * 0.65, sh * 0.45, sw * 0.25, sh * 0.35, 0, sh * 0.5);
+    greyPath.close();
     paint.color = Colors.grey.shade800;
-    canvas.drawPath(greyWave, paint);
-
-    // Yellow
-    Path yellowWave = Path();
-    yellowWave.lineTo(sw * 0.7, 0);
-    yellowWave.cubicTo(
-        sw * 0.6, sh * 0.05, sw * 0.27, sh * 0.01, sw * 0.18, sh * 0.12);
-    yellowWave.quadraticBezierTo(sw * 0.12, sh * 0.2, 0, sh * 0.2);
-    yellowWave.close();
-    paint.color = Colors.orange.shade300;
-    canvas.drawPath(yellowWave, paint);
+    canvas.drawPath(greyPath, paint);
   }
 
   @override
